4e7f7ecf33526cd22849b29cd29a273710d2ce38
AdamWojtczak <adamwojtczak807@gmail.com>
2020-07-09 16:22:33 +0200

    feature1_impl WhenCalculatingArrivalTimes tests implemented
    Signed-off-by: AdamWojtczak <adamwojtczak807@gmail.com>

--- src/test/java/edu/iis/mto/bdd/trains/junit/WhenCalculatingArrivalTimes.java
+++ src/test/java/edu/iis/mto/bdd/trains/junit/WhenCalculatingArrivalTimes.java
@@ -2,68 +2,101 @@ package edu.iis.mto.bdd.trains.junit;
 
 import edu.iis.mto.bdd.trains.cucumber.steps.JodaLocalTimeConverter;
 import edu.iis.mto.bdd.trains.model.Line;
-import edu.iis.mto.bdd.trains.services.InterinaryService;
-import edu.iis.mto.bdd.trains.services.InterinaryServiceImpl;
+import edu.iis.mto.bdd.trains.services.ItineraryService;
+import edu.iis.mto.bdd.trains.services.ItineraryServiceImplementation;
 import edu.iis.mto.bdd.trains.services.TimetableService;
 import org.joda.time.LocalTime;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class WhenCalculatingArrivalTimes {
-
-    InterinaryService interinaryService;
+    ItineraryService itineraryService;
     TimetableService timetableService;
+    private String departure = "Radom",
+            destination = "Wałbrzych";
+    private String[] constStations = {departure, "Kielce", "Ostrowiec", destination};
+    private Line line = Line.named("Line123")
+            .departingFrom(departure)
+            .withStations(constStations);
+    private List<LocalTime> trainsTimes;
+    List<LocalTime> expectedTrainsTimes;
+    private JodaLocalTimeConverter jodaLocalTimeConverter = new JodaLocalTimeConverter();
+    LocalTime startingTime = jodaLocalTimeConverter.transform("5:00");
 
     @Before
     public void setUp() {
         timetableService = mock(TimetableService.class);
-        interinaryService = new InterinaryServiceImpl(timetableService);
+        itineraryService = new ItineraryServiceImplementation(timetableService);
+        trainsTimes = new ArrayList<>();
+        expectedTrainsTimes = new ArrayList<>();
+        when(timetableService.findLinesThrough(departure, destination))
+                .thenReturn(Collections.singletonList(line));
     }
 
     @Test
-    public void shouldGiveMeInfoAbout3PossibleDepartueTimes() {
-        String departure = "Warszawa";
-        String destination = "Gdańsk";
-        Line firstLine = Line.named("Pl")
-                .departingFrom("Warszawa")
-                .withStations("Warszawa", "Poznan", "Lodz", destination);
-        Line secondLine = Line.named("Pl2")
-                .departingFrom("Warszawa")
-                .withStations("Warszawa", "Poznan", "Lodz", destination);
-        when(timetableService.findLinesThrough(anyString(), anyString()))
-                .thenReturn(List.of(firstLine, secondLine));
+    public void shouldReturnNoneDepartureTimes() {
+        trainsTimes = new ArrayList<LocalTime>() {{
+            add(new LocalTime("4:50"));
+            add(new LocalTime("4:59"));
+            add(new LocalTime("5:16"));
+            add(new LocalTime("5:17"));
+            add(new LocalTime("6:00"));
+        }};
 
-        JodaLocalTimeConverter jodaLocalTimeConverter = new JodaLocalTimeConverter();
+        when(timetableService.findArrivalTimes(line, departure))
+                .thenReturn(trainsTimes);
 
+        List<LocalTime> resultTimes = itineraryService.findNextDepartures(departure, destination, startingTime);
 
-        when(timetableService.findArrivalTimes(firstLine, departure))
-                .thenReturn(List.of(
-                        jodaLocalTimeConverter.transform("8:15"),
-                        jodaLocalTimeConverter.transform("8:35"),
-                        jodaLocalTimeConverter.transform("9:00")
-                ));
+        assertEquals(0, resultTimes.size());
+    }
 
-        when(timetableService.findArrivalTimes(secondLine, departure))
-                .thenReturn(List.of(
-                        jodaLocalTimeConverter.transform("8:12"),
-                        jodaLocalTimeConverter.transform("8:20"),
-                        jodaLocalTimeConverter.transform("9:00")
-                ));
+    @Test
+    public void shouldReturn2DepartureTimes() {
+        trainsTimes = new ArrayList<LocalTime>() {{
+            add(new LocalTime("4:50"));
+            add(new LocalTime("5:06"));
+            add(new LocalTime("5:14"));
+            add(new LocalTime("5:17"));
+            add(new LocalTime("6:00"));
+        }};
 
-        List<LocalTime> localTimes = interinaryService.findNextDepartures(departure, destination, jodaLocalTimeConverter.transform("8:10"));
-        List<LocalTime> expectedLocalTimes = List.of(
-                jodaLocalTimeConverter.transform("8:12"),
-                jodaLocalTimeConverter.transform("8:15"),
-                jodaLocalTimeConverter.transform("8:20")
-        );
+        expectedTrainsTimes = new ArrayList<LocalTime>() {{
+            add(new LocalTime("5:06"));
+            add(new LocalTime("5:14"));
+        }};
 
-        assertEquals(expectedLocalTimes, localTimes);
+        when(timetableService.findArrivalTimes(line, departure))
+                .thenReturn(trainsTimes);
+
+        List<LocalTime> resultTimes = itineraryService.findNextDepartures(departure, destination, startingTime);
+
+        assertEquals(expectedTrainsTimes, resultTimes);
+    }
+
+    @Test
+    public void shouldReturnAllDepartureTimes() {
+        trainsTimes = new ArrayList<LocalTime>() {{
+            add(new LocalTime("5:01"));
+            add(new LocalTime("5:03"));
+            add(new LocalTime("5:05"));
+            add(new LocalTime("5:07"));
+            add(new LocalTime("5:09"));
+        }};
+
+        when(timetableService.findArrivalTimes(line, departure))
+                .thenReturn(trainsTimes);
+
+        List<LocalTime> resultTimes = itineraryService.findNextDepartures(departure, destination, startingTime);
+
+        assertEquals(trainsTimes, resultTimes);
     }
 }
